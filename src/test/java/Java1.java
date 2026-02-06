82c9100144774cba5dfde8bb0951ccdd681f1bc3
Sandhya Gyawali <sandhyagyawali63@gmail.com>
2026-02-06 15:43:29 +0545

    message

--- src/test/java/Java1.java
+++ src/test/java/Java1.java
@@ -1,5 +1,10 @@
 public class Java1 {
     public static void main(String[] args){
         System.out.println("Heelo");
+        System.out.println("Make some changes");
+
+        var m = 12;
+        var n = 14;
+        System.out.println(m+n);
     }
 }
